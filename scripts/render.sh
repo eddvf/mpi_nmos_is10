8c9278e6e40093726eecd494658baeb7356d1ed6
Endrit Vorfaj <endrit.vorfaj@mpi-sp.org>
2025-12-15 19:26:45 +0100

    keycloak personalized still

--- scripts/render.sh
+++ scripts/render.sh
@@ -7,7 +7,9 @@ mkdir -p nginx bind/config json_configs keycloak/import
 
 envsubst '${REGISTRY_HOST} ${REGISTRY_IP} ${NODE_HOST} ${NODE_IP} ${KEYCLOAK_HOST} ${KEYCLOAK_IP} ${KEYCLOAK_REALM}' < templates/nginx.conf.tpl > nginx/nginx.conf
 envsubst < templates/db.domain.tpl > "bind/config/db.${DOMAIN}"
-envsubst < templates/nmos-realm.json.tpl > keycloak/import/nmos-realm.json
+
+rm -f keycloak/import/*.json
+envsubst < templates/nmos-realm.json.tpl > "keycloak/import/${KEYCLOAK_REALM}-realm.json"
 
 echo "; reverse placeholder" > bind/config/db.reverse
 envsubst < templates/node.json.tpl > json_configs/node.json
